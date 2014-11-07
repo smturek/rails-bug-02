f656f968eb940e3cb2f794a5819e9703a81254e4
smturek <smturek@gmail.com>
2014-11-07 11:47:31 -0700

    fixed login bug

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,17 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'Users should be logged in after registering' do
+    visit root_path
+    within(".auth") { click_on "Register" }
+    fill_in "Name", with: "John"
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password", with: "test"
+    fill_in "Confirm", with: "test"
+    within(".registration-form") { click_on "Register" }
+
+    expect(page).to have_no_content("Login")
+    expect(page).to have_content("Logout")
+  end
+
 end
